25481013742630fdc928c28b4bb3743eb1431cf3
Raquel <raquel@Raquels-Air.lan>
2021-11-10 18:27:12 +0000

    DFA union implemented with simple tests

---
+++ dfa_operations.py
@@ -0,0 +1,30 @@
+import itertools
+from DFA import DFA
+
+def union(dfa1, dfa2):
+    states = set(itertools.product(dfa1.states, dfa2.states))
+    alphabet = set.union(dfa1.alphabet, dfa2.alphabet)
+
+    transitions = dict()
+    for state in states:
+        transitions[state] = dict()
+        for char in alphabet:
+
+            reach_state1 = None
+            if state[0] != None and char in dfa1.transitions[state[0]]:
+                reach_state1 = dfa1.transitions[state[0]][char]
+
+            reach_state2 = None
+            if state[1] != None and char in dfa2.transitions[state[1]]:
+                reach_state2 = dfa2.transitions[state[1]][char]
+            
+            transitions[state][char] = (reach_state1, reach_state2)
+
+    
+    start_state = (dfa1.start_state, dfa2.start_state)
+    accepting_states = set.union(
+        set(itertools.product(dfa1.accepting_states, dfa2.states)),
+        set(itertools.product(dfa1.states, dfa2.accepting_states))
+        )
+    
+    return DFA(states, alphabet, transitions, start_state, accepting_states)
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from DFA import DFA
+import dfa_operations
 
-states = {0, 1, 2}
-alphabet = {0}
+# DFA 1 - accepts strings with a number of 0s multiple of 3
+states = {0, 1, 2, None}
+alphabet = {'0'}
 transitions = {
     0: {'0' : 1},
     1: {'0' : 2},
@@ -10,25 +12,74 @@ transitions = {
 start_state = 0
 accepting_states = {0}
 
-dfa = DFA(states, alphabet, transitions, start_state, accepting_states)
+dfa1 = DFA(states, alphabet, transitions, start_state, accepting_states)
+
+# DFA 2 - accepts strings with a number of 0s multiple of 2
+states = {0, 1, None}
+alphabet = {'0'}
+transitions = {
+    0: {'0' : 1},
+    1: {'0' : 0}
+    }
+start_state = 0
+accepting_states = {0}
+
+dfa2 = DFA(states, alphabet, transitions, start_state, accepting_states)
+
+# DFA 3 - accepts strings with a number of 1s multiple of 2
+states = {0, 1, None}
+alphabet = {'1'}
+transitions = {
+    0: {'1' : 1},
+    1: {'1' : 0}
+    }
+start_state = 0
+accepting_states = {0}
+
+dfa3 = DFA(states, alphabet, transitions, start_state, accepting_states)
 
 
 def dfa_accepts_string():
-    assert dfa.accepts("") == True
-    assert dfa.accepts("0") == False
-    assert dfa.accepts("00") == False
-    assert dfa.accepts("000") == True
-    assert dfa.accepts("0000") == False
+    assert dfa1.accepts("") == True
+    assert dfa1.accepts("0") == False
+    assert dfa1.accepts("00") == False
+    assert dfa1.accepts("000") == True
+    assert dfa1.accepts("0000") == False
 
 def dfa_reaches_state():
-    assert dfa.reaching_state("") == 0
-    assert dfa.reaching_state("0") == 1
-    assert dfa.reaching_state("00") == 2
-    assert dfa.reaching_state("000") == 0
-    assert dfa.reaching_state("0000") == 1
+    assert dfa1.reaching_state("") == 0
+    assert dfa1.reaching_state("0") == 1
+    assert dfa1.reaching_state("00") == 2
+    assert dfa1.reaching_state("000") == 0
+    assert dfa1.reaching_state("0000") == 1
 
+def dfa_union_same_alphabet():
+    union = dfa_operations.union(dfa1, dfa2)
+    assert union.accepts("") == True
+    assert union.accepts("0") == False
+    assert union.accepts("00") == True
+    assert union.accepts("000") == True
+    assert union.accepts("0000") == True
+    assert union.accepts("00000") == False
+
+def dfa_union_different_alphabet():
+    union = dfa_operations.union(dfa1, dfa3)
+    assert union.accepts("") == True
+    assert union.accepts("0") == False
+    assert union.accepts("00") == False
+    assert union.accepts("000") == True
+    assert union.accepts("0000") == False
+    assert union.accepts("00000") == False
+    assert union.accepts("000000") == True
+    assert union.accepts("11111") == False
+    assert union.accepts("1111") == True
+    assert union.accepts("111") == False
+    assert union.accepts("11") == True
+    assert union.accepts("1") == False
 
 if __name__ == "__main__":
     dfa_accepts_string()
     dfa_reaches_state()
+    dfa_union_same_alphabet()
+    dfa_union_different_alphabet()
     print("Everything passed")
