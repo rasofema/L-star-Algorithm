d939c77a2cac9a38c66cea86c9f927b198f801cd
Raquel <raquel@Raquels-MacBook-Air.local>
2021-11-17 23:48:52 +0000

    oracle: changed max size of test string

--- oracle.py
+++ oracle.py
@@ -10,7 +10,7 @@ class oracle():
     def hypothesis_is_correct(self, hypothesis):
         alphabet = list(self.dfa.alphabet)
         for _ in range(1000):
-            test_string = random.choices(alphabet, k=random.randint(0, 3))
+            test_string = random.choices(alphabet, k=random.randint(0, 1000))
             if hypothesis.accepts(test_string) != self.dfa.accepts(test_string):
                 return test_string
         return True
